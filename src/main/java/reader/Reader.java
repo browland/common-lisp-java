0ec25de74bad9d63ff125709b6846174231cd582
Ben Rowland <ben.rowland@gmail.com>
2025-11-14 23:55:31 +0000

    Trying to break things up

--- src/main/java/reader/Reader.java
+++ src/main/java/reader/Reader.java
@@ -8,16 +8,15 @@ public class Reader {
             '\'', '#', '`'
     );
 
+    private final StringBuilder prefixBuilder = new StringBuilder();
+    private final StringBuilder atomStringBuilder = new StringBuilder();
+
+    private boolean readPrefix;
+    private RList.Builder listBuilder;
+    private int depth;
+    private String prefix;
+
     public RList read(String program) {
-        boolean readPrefix = false;
-        StringBuilder prefixBuilder = new StringBuilder();
-        StringBuilder atomStringBuilder = new StringBuilder();
-
-        RList.Builder listBuilder = null;
-
-        int depth = 0;
-        String prefix = null;
-
         for(char c : program.toCharArray()) {
             if(!readPrefix) {
                 if(PREFIX_CHARS.contains(c)) {
@@ -32,49 +31,13 @@ public class Reader {
 
             // now deal with atom or list
             if(c == '(') {
-                RList.Builder tempListBuilder = new RList.Builder()
-                        .parentListBuilder(listBuilder)
-                        .depth(depth)
-                        .prefix(prefix);
-
-                readPrefix = false;  // whenever we consume 'prefix' we need to reset the flag to say prefix has been read
-                prefixBuilder.delete(0, prefixBuilder.length());  // whenever we consume 'prefix' we need to reset the flag to say prefix has been read
-
-                if(listBuilder != null) {
-                    listBuilder.addNodeBuilder(tempListBuilder);
-                }
-                listBuilder = tempListBuilder;
-                depth++;
+                startList();
             }
             else if(c == ')') {
-                if(!atomStringBuilder.isEmpty()) {
-                    Atom.Builder atomBuilder = new Atom.Builder()
-                            .value(atomStringBuilder.toString())
-                            .prefix(prefix);
-                    listBuilder.addNodeBuilder(atomBuilder);
-                    atomStringBuilder.delete(0, atomStringBuilder.length());
-                }
-
-                listBuilder = listBuilder.getParentListBuilder() != null ? listBuilder.getParentListBuilder() : listBuilder;
-                depth--;
+                endList();
             }
             else if(c == ' ') {
-                // if we don't have any characters from an atom then we may have just completed parsing a list
-                if(atomStringBuilder.isEmpty()) {
-                    readPrefix = false;  // Todo this feels like a bit of a hack; we should be clearing this flag earlier, not when we've processed a list and we're now on the space after it
-                    continue;
-                }
-
-                String atom = atomStringBuilder.toString();
-                Atom.Builder atomBuilder = new Atom.Builder()
-                        .value(atom)
-                        .prefix(prefix);
-                atomStringBuilder.delete(0, atomStringBuilder.length());
-
-                readPrefix = false;  // whenever we consume 'prefix' we need to reset the flag to say prefix has been read
-                prefixBuilder.delete(0, atomStringBuilder.length());
-
-                listBuilder.addNodeBuilder(atomBuilder);  // todo need to collect the prefix into whatever we add here, not just a string
+                endNode();
             }
             else {
                 if(c != '\n') {  // todo check for whitespace char instead
@@ -84,6 +47,54 @@ public class Reader {
         }
 
         return listBuilder.build();
+    }
+
+    private void endNode() {
+        // if we don't have any characters from an atom then we may have just completed parsing a list
+        if(atomStringBuilder.isEmpty()) {
+            readPrefix = false;  // Todo this feels like a bit of a hack; we should be clearing this flag earlier, not when we've processed a list and we're now on the space after it
+            return;
+        }
+
+        String atom = atomStringBuilder.toString();
+        Atom.Builder atomBuilder = new Atom.Builder()
+                .value(atom)
+                .prefix(prefix);
+        atomStringBuilder.delete(0, atomStringBuilder.length());
+
+        readPrefix = false;  // whenever we consume 'prefix' we need to reset the flag to say prefix has been read
+        prefixBuilder.delete(0, atomStringBuilder.length());
+
+        listBuilder.addNodeBuilder(atomBuilder);  // todo need to collect the prefix into whatever we add here, not just a string
+    }
+
+    private void endList() {
+        if(!atomStringBuilder.isEmpty()) {
+            Atom.Builder atomBuilder = new Atom.Builder()
+                    .value(atomStringBuilder.toString())
+                    .prefix(prefix);
+            listBuilder.addNodeBuilder(atomBuilder);
+            atomStringBuilder.delete(0, atomStringBuilder.length());
+        }
+
+        listBuilder = listBuilder.getParentListBuilder() != null ? listBuilder.getParentListBuilder() : listBuilder;
+        depth--;
+    }
+
+    private void startList() {
+        RList.Builder tempListBuilder = new RList.Builder()
+                .parentListBuilder(listBuilder)
+                .depth(depth)
+                .prefix(prefix);
+
+        readPrefix = false;  // whenever we consume 'prefix' we need to reset the flag to say prefix has been read
+        prefixBuilder.delete(0, prefixBuilder.length());  // whenever we consume 'prefix' we need to reset the flag to say prefix has been read
+
+        if(listBuilder != null) {
+            listBuilder.addNodeBuilder(tempListBuilder);
+        }
+        listBuilder = tempListBuilder;
+        depth++;
     }
 
 
