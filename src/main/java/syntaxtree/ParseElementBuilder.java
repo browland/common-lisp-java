1da89f2bc2ef8cf1212ab3d218eab1ebb5c9ceea
Ben Rowland <ben.rowland@gmail.com>
2025-12-24 17:40:47 +0000

    Small cleanups and comments

--- src/main/java/syntaxtree/ParseElementBuilder.java
+++ src/main/java/syntaxtree/ParseElementBuilder.java
@@ -4,13 +4,16 @@ import reader.CharacterReaderEvent;
 
 /**
  * This is more complex than the layer below (CharacterReader).
- * This receives character-level events and builds complete atoms and lists incrementally as events are received.
- * For this reason, it maintains a fair bit of state.
+ * This receives character-level events, then gathers prefixes, and detects the start and end of lists as events are incrementally received
+ * from the CharacterReader below.
+ * It then fires off higher level events to the layer above - these events represent an entire atom, prefix, or start/end of a list.
  */
 public class ParseElementBuilder {
+    // The job of this layer is to gather state for a prefix, suffix, or atom into the appropriate builder.
     private final StringBuilder prefixBuilder = new StringBuilder();
     private final StringBuilder suffixBuilder = new StringBuilder();
     private final StringBuilder atomStringBuilder = new StringBuilder();
+
     private final SyntaxTreeBuilder syntaxTreeBuilder;
 
     public ParseElementBuilder(SyntaxTreeBuilder syntaxTreeBuilder) {
@@ -25,6 +28,9 @@ public class ParseElementBuilder {
     }
 
     public void onQuoteChar(CharacterReaderEvent event) {
+        // Because the atomStringBuilder is only cleared at the end of a list or on whitespace (or upon reset, which
+        // is called externally and is a bit of a code-smell - shouldn't need it?) then we can determine if this quote
+        // character is a prefix or suffix by whether there are characters present in the atomStringBuilder.
         if(atomStringBuilder.isEmpty()) {
             prefixBuilder.append(event.character());
         }
@@ -34,12 +40,17 @@ public class ParseElementBuilder {
     }
 
     public void onWhitespace(CharacterReaderEvent event) {
-        // if we don't have any characters from an atom (including prefix) then we may have just completed parsing a list
+        // if we don't have any characters from an atom then by elimination this is a space after the end of a list.
+        // We already handled the end of the list (when we saw the close-bracket) so there is nothing we need to do.
+        // However, we also need to check for the lack of a prefix, as we may be handling a string (double-quoted) beginning
+        // with 1 or more spaces.  This is why we treat double-quote specially (as a quote char) even though it's not
+        // a quote char in terms of lisp syntax.  Maybe we could break it out somehow, but for now this works.
         if(atomStringBuilder.isEmpty() && prefixBuilder.isEmpty()) {
             return;
         }
 
-        // if we're within a string literal (seen open-quote but not close-quote) then treat space as just another character for the current atom
+        // If we're within a string literal (seen open-quote but not close-quote) then treat space as just another
+        // character for the current atom
         if(!prefixBuilder.isEmpty() && "\"".equals(prefixBuilder.toString()) && suffixBuilder.isEmpty()) {
             atomStringBuilder.append(event.character());
             return;
@@ -50,27 +61,29 @@ public class ParseElementBuilder {
 
         String prefix = prefixBuilder.length() != 0 ? prefixBuilder.toString() : null;
         String suffix = suffixBuilder.length() != 0 ? suffixBuilder.toString() : null;
+
         Atom.Builder atomBuilder = new Atom.Builder()
                 .value(atom)
                 .prefix(prefix)
                 .suffix(suffix);
-        atomStringBuilder.delete(0, atomStringBuilder.length());
+        syntaxTreeBuilder.newAtom(atomBuilder);
 
+        atomStringBuilder.delete(0, atomStringBuilder.length());
         prefixBuilder.delete(0, prefixBuilder.length());
         suffixBuilder.delete(0, suffixBuilder.length());
-
-        syntaxTreeBuilder.newAtom(atomBuilder);
     }
 
     public void endList(CharacterReaderEvent event) {
         if(!atomStringBuilder.isEmpty()) {
             String prefix = prefixBuilder.length() != 0 ? prefixBuilder.toString() : null;
             String suffix = suffixBuilder.length() != 0 ? suffixBuilder.toString() : null;
+
             Atom.Builder atomBuilder = new Atom.Builder()
                     .value(atomStringBuilder.toString())
                     .prefix(prefix)
                     .suffix(suffix);
             syntaxTreeBuilder.newAtom(atomBuilder);
+
             atomStringBuilder.delete(0, atomStringBuilder.length());
         }
         syntaxTreeBuilder.endList();
@@ -79,9 +92,7 @@ public class ParseElementBuilder {
     public void startList(CharacterReaderEvent event) {
         // Determine the entire prefix - may be null, 1 or 2 characters based on current knowledge
         String prefix = prefixBuilder.length() != 0 ? prefixBuilder.toString() : null;
-
         syntaxTreeBuilder.startList(event.depth()-1, prefix);
-
         prefixBuilder.delete(0, prefixBuilder.length());  // whenever we consume 'prefix' we need to reset the flag to say prefix has been read
     }
 
