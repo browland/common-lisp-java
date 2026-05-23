fdcc776e3f52f3917dac1604039034ce018e077b
Ben Rowland <ben.rowland@gmail.com>
2026-05-23 17:53:34 +0100

    setf now working with car accessor!

--- lisp-sources/towards-setf.lisp
+++ lisp-sources/towards-setf.lisp
@@ -1,5 +1,12 @@
 (defmacro setf (place value)
-  (if (symbolp place)
-    `(setq ,place ,value)
-  )
-)
+  (cond
+    ((symbolp place)
+     `(setq ,place ,value))
+
+    ((and (listp place)
+          (eq (car place) 'car))
+     `(rplaca ,(cadr place) ,value))))
+
+(defvar *x* '(1 2))
+(setf (car *x*) 2)
+*x*
