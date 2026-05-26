c5cbdcb9574f398e541b8e0ae836f359e3b1840d
Ben Rowland <ben.rowland@gmail.com>
2026-05-26 21:52:30 +0100

    Even number list filter function implemented better

--- lisp-sources/filter.lisp
+++ lisp-sources/filter.lisp
@@ -1,5 +1,5 @@
 ; https://cs.stanford.edu/people/nick/compdocs/LISP_Examples.pdf
-(defmacro mydolist (mylist func)
+(defmacro myfilter (mylist func)
   `(let ((curr nil)
         (rest ,mylist)
         (result nil))
@@ -7,17 +7,11 @@
       (tagbody
         start
         (setq curr (car rest))
-        (setq result (funcall ,func curr result))
+        (if (funcall ,func curr) (setq result (cons curr result)))
         (setq rest (cdr rest))
-        (if (eq rest nil) (return-from myloop (funcall ,func curr result)))
+        (if (eq rest nil) (return-from myloop result))
         (go start)))))
 
-; this is all very janky but working - needs improving
 (defvar mylist '(1 2 3))
-(let ((fxn #'evenp)
-      (resultingcons nil))
-  (defvar predicate (lambda (x y)
-    (if (and x (funcall fxn x)) (setq resultingcons (cons x resultingcons)) (setq resultingcons resultingcons))))
-  (mydolist mylist predicate)
-  (format t resultingcons))
+(format t (myfilter mylist #'evenp))
 
