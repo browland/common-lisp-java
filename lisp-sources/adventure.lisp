e9442b202e3aed58d118b9222ff17f5d7ef11eb5
Ben Rowland <ben.rowland@gmail.com>
2026-05-30 06:57:40 +0100

    simplify data for adventure example

--- lisp-sources/adventure.lisp
+++ lisp-sources/adventure.lisp
@@ -1,26 +1,20 @@
 (defvar *nodes*
-  (list
-   (cons 'start
-         '(:text "You wake up in a room. Left or right?"
+  '((start :text "You wake up in a room. Left or right?"
            :choices (("left" . monster)
-                      ("right" . door))))
+                     ("right" . door)))
 
-   (cons 'monster
-         '(:text "A monster kills you."
-           :choices nil))
+    (monster :text "A monster kills you."
+             :choices nil)
 
-   (cons 'door
-         '(:text "A steel door blocks your way. Key? (yes/no)"
-           :choices (("yes" . escape)
-                      ("no" . trapped))))
+    (door :text "A steel door blocks your way. Key? (yes/no)"
+          :choices (("yes" . escape)
+                    ("no" . trapped)))
 
-   (cons 'escape
-         '(:text "You escape. You win."
-           :choices nil))
+    (escape :text "You escape. You win."
+            :choices nil)
 
-   (cons 'trapped
-         '(:text "You are trapped. The monster finds you."
-           :choices nil))))
+    (trapped :text "You are trapped. The monster finds you."
+             :choices nil)))
 
 (defun get-node (name)
   (cdr (assoc name *nodes*)))
