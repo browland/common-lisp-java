cf21d3c09731a4976ade7f5ccedf40017735c1d0
Ben Rowland <ben.rowland@gmail.com>
2026-07-10 20:10:07 +0100

    Comments only

--- lisp-sources/lib/4-files.lisp
+++ lisp-sources/lib/4-files.lisp
@@ -3,4 +3,8 @@
             ,@forms
             (close ,sref_var)))
 
-; todo implement do, and then implement read-line
+; todo implement read-line
+;      we need: a coerce function to turn cons list of chars into a string
+;               (maybe a stream function to pass stream values around rather than assuming stdin, stdout or always working with files)
+;               :eof symbol returned from readchar at eof
+;               #\Newline char literal if we don't already have it
